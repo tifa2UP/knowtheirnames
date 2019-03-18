35b70d4b21c47b4fe7d520a19e8798ea02198dc0
Azizur Rahman <azizur@prodevstudio.com>
2019-03-18 00:14:47 +0000

    Implement share bar on each story

--- src/components/BoxDescription/index.js
+++ src/components/BoxDescription/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+import './share-bar.css';
+
 export default class BoxDescription extends Component {
   render() {
     return (
@@ -11,10 +13,18 @@ export default class BoxDescription extends Component {
               <h2>{this.props.name}</h2>
               <p>{this.props.notes}</p>
             </div>
+            <div className="share-bar">
+              <ul className="vertical medium-horizontal menu">
+                <li><a href="#0"><i className="fa fab fa-facebook-square"></i> <span>Share on Facebook</span></a></li>
+                <li><a href="#0"><i className="fa fab fa-twitter-square"></i> <span>Share on Twitter</span></a></li>
+                <li><a href="#0"><i className="fa fab fa-linkedin"></i> <span>Share on LinkedIn</span></a></li>
+                <li><a href="#0"><i className="fa fas fa-link"></i> <span>Copy Link</span></a></li>
+              </ul>
+            </div>
           </div>
         </div>
-         <nav>
-          <a href="#" id="back" style={{color: "#fff"}} onClick={this.props.onClose}>
+        <nav>
+          <a href="#" id="back" style={{ color: "#fff" }} onClick={this.props.onClose}>
             <i className="fa fa-close" />
           </a>
         </nav>
