2ddf630a475bedc64291c79a1bea73d09bc1f459
Abdellatif Abdelfattah <ab@alcatraz.ai>
2019-03-17 18:20:34 -0700

    Add placeholder donations pill

--- src/components/BoxDescription/index.js
+++ src/components/BoxDescription/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import "./share-bar.css";
 
-import './share-bar.css';
+const ContributeDiv = styled.div`
+  visibility: visible;
+  position: absolute;
+  top: 10%;
+  right: 10%;
+  z-index: 2;
+  padding-right: 10%;
+  color: #fff;
+`;
 
 export default class BoxDescription extends Component {
   getNotes = () => {
@@ -11,21 +20,43 @@ export default class BoxDescription extends Component {
       return <a href="">share their story</a>;
     }
   };
+
   render() {
     return (
       <div>
         <div className="header-title invisible">
+        {/* <ContributeDiv className="invisible"> contribute to this profile</ContributeDiv> */}
           <div className="row">
+            <div />
             <div className="large-8 columns">
               <h2>{this.props.name}</h2>
               <p>{this.getNotes()}</p>
             </div>
             <div className="share-bar">
               <ul className="vertical medium-horizontal menu">
-                <li><a href="#0"><i className="fa fab fa-facebook-square"></i> <span>Share on Facebook</span></a></li>
-                <li><a href="#0"><i className="fa fab fa-twitter-square"></i> <span>Share on Twitter</span></a></li>
-                <li><a href="#0"><i className="fa fab fa-linkedin"></i> <span>Share on LinkedIn</span></a></li>
-                <li><a href="#0"><i className="fa fas fa-link"></i> <span>Copy Link</span></a></li>
+                <li>
+                  <a href="#0">
+                    <i className="fa fab fa-facebook-square" />{" "}
+                    <span>Share on Facebook</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#0">
+                    <i className="fa fab fa-twitter-square" />{" "}
+                    <span>Share on Twitter</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#0">
+                    <i className="fa fab fa-linkedin" />{" "}
+                    <span>Share on LinkedIn</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#0">
+                    <i className="fa fas fa-link" /> <span>Copy Link</span>
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,51 @@
 import React, { Component } from "react";
 import Box from "../components/Box";
 import { format } from "url";
+import styled from "styled-components";
+
+const DonationsPillStyle = styled.a`
+  position: fixed;
+  bottom: 44px;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  color: white;
+  background: #54b54a;
+  padding: 20px;
+  width: fit-content;
+  border-radius: 90px;
+  box-shadow: 0 8px 18px 0 rgba(34, 49, 89, 0.1);
+  cursor: pointer;
+  text-decoration: none !important;
+`;
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeBox: false
+      activeBox: false,
+      donateInvisible: true
     };
   }
-  shuffle = (array) => {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
+  shuffle = array => {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
+
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
-  }
+  };
 
   getBoxDimensions(size) {
     let result = [];
@@ -54,11 +74,21 @@ class HomePage extends Component {
   };
 
   onClose = () => {
-    console.log("parent onClose called")
+    console.log("parent onClose called");
     this.setState({
       activeBox: false
     });
-    this.forceUpdate()
+    this.forceUpdate();
+  };
+
+  onScroll = () => {
+    console.log(document.documentElement.scrollTop )
+    if (document.documentElement.scrollTop < 100) {
+    } else {
+      this.setState({
+        donateInvisible: false
+      });
+    }
   };
   render() {
     let array = require("../memorial.json");
@@ -81,14 +111,30 @@ class HomePage extends Component {
         />
       </div>
     ));
+
+    const DonationsPill = (
+      <DonationsPillStyle
+        className={`appear ${
+          this.state.donateInvisible ? "invisible" : "notinvisible"
+        }`}
+        target="_blank"
+        href="https://www.launchgood.com/project/support_for_the_families__victims_of_the_new_zealand_mosque_shootings?src=NZshooting&utm_source=Homepagebanner&utm_medium=1&utm_campaign=NZShooting#!/"
+      >
+        Donate to the families
+      </DonationsPillStyle>
+    );
     return (
-      <div>
+      <div onScroll={this.onScroll}>
         <div className="logo">
-          <a href="/" style={{ color: "white", textDecoration: "none !important" }}>
+          <a
+            href="/"
+            style={{ color: "white", textDecoration: "none !important" }}
+          >
             KNOW THEIR NAMES.
           </a>
         </div>
         {BoxRenders}
+        {DonationsPill}
       </div>
     );
   }
